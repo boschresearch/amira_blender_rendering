6f4557e0b24df564e9e2c811101e7b7b5392b380
Shapiro Yoel (CR/RTO-IL) <shy1tv@bosch.com>
2019-07-18 15:57:38 +0300

    1st commit. consider removing non-source files (.blnd) and pulling from elsewhere during build

---
+++ scripts/part_in_hand_frames_demo.py
@@ -0,0 +1,147 @@
+# FIXME: workout using Blender (bpy) as a python module
+"""For running within Blender python console copy-paste the lines below:
+
+import os.path as osp
+import sys
+pkg_dir = "/home/yoelsh/work/amira_tools/amira_blender_rendering" #TODO: customize to your PC
+sys.path.append(osp.join(pkg_dir, "src"))
+sys.path.append(osp.join(pkg_dir, "scripts"))
+import part_in_hand_frames_demo
+panda = part_in_hand_frames_demo.run()
+"""
+import os.path as osp
+import time
+import numpy as np
+
+import bpy
+from mathutils import Vector, Matrix
+
+from amira_blender_rendering import robot_driver, utils
+from amira_blender_rendering import blender_utils as blnd
+
+scripts_dir = utils.get_my_dir(__file__)
+pkg_dir = osp.split(scripts_dir)[0]
+out_dir = osp.join(pkg_dir, "out")
+output_file = osp.join(out_dir, "image_{:04}")
+
+# utils.try_makedirs(out_dir)
+# src_dir = osp.join(pkg_dir, "src", "amira_blender_rendering")
+# assets_dir = osp.join(src_dir, "assets")
+
+
+def scene_setup():
+
+    scene = bpy.context.scene
+    scene.render.engine = 'CYCLES'
+    scene.frame_start = 1
+    scene.frame_current = 1
+    scene.frame_end = 10
+
+    render = scene.render
+    render.filepath = osp.join(out_dir, "panda_demo_")
+    render.image_settings.file_format = "JPEG"
+    render.resolution_x = 640
+    render.resolution_y = 480
+
+    try:
+        scene.cycles.device = 'GPU'
+        render.resolution_x = 1280
+        render.resolution_y = 960
+    except TypeError:
+        pass
+
+    # Assuming single layer
+    try:
+        layer = render.layers[0]
+        layer.cycles.use_denoising = True
+        layer.cycles.denoising_radius = 5
+        layer.cycles.denoising_strength = 0.3
+        layer.cycles.denoising_feature_strength = 0.3
+        layer.cycles.denoising_diffuse_direct = False
+        layer.cycles.denoising_transmission_direct = False
+        layer.cycles.denoising_subsurface_direct = False
+    except Exception as err:
+        print("failed to set denoising")
+        print(err)
+
+    return scene, render
+
+
+def run():
+
+    # timestamp = time.strftime("%Y%b%d-%H%M%S")
+
+    scene, render = scene_setup()
+
+    blnd.clear_all_objects()
+    blnd.create_room_corner()
+
+    # lighting
+    bpy.ops.object.lamp_add(
+        type='SUN',
+        radius=1,
+        view_align=False,
+        location=(1, 2, 2),
+    )
+    sun_name = bpy.data.lamps[-1].name
+    sun = bpy.data.objects[sun_name]
+    sun.name = "Sun"
+    sun.rotation_mode = 'XYZ'
+    sun.rotation_euler.x = -0.2
+
+    # camera
+    bpy.ops.object.camera_add(
+        enter_editmode=False,
+        location=(1.2, 1.2, 1.4),
+        rotation=(np.deg2rad(60), 0, np.deg2rad(135)),
+    )
+    cam_name = bpy.data.cameras[0].name
+    cam = bpy.data.objects[cam_name]
+    cam.name = "Camera_1"
+    cam.scale = Vector((0.15, 0.15, 0.15))
+
+    scene.camera = cam
+
+    # robot
+    panda = robot_driver.PandaIKDriver()
+
+    # eBike parts
+    gear = blnd.load_cad_part("Gear-Wheel")
+    blnd.translate_object(gear, (0.45, -0.2, 0.0))
+
+    shaft = blnd.load_cad_part("Shaft")
+    shaft_bbox = blnd.get_mesh_bounding_box(shaft)
+
+    panda.gripper.set_to(shaft_bbox.x.max)
+
+    # images
+    for k_img in range(1, 9):
+
+        if k_img > 1:
+            panda.randomize_tcp()
+
+        scene.update()  # must update panda.tcp.matrix_world
+        shaft.matrix_world = panda.tcp.matrix_world
+        if k_img > 3:
+
+            panda.gripper.set_to(shaft_bbox.y.max)
+
+            # rotating part inside gripper
+            H = np.array(shaft.matrix_world)
+            R_new = np.dot(H[:3, :3], Matrix.Rotation(np.deg2rad(90), 3, (0, 1, 0)))
+            H[:3, :3] = R_new
+            shaft.matrix_world = Matrix(H)
+            # if this breaks, try fall-back to the command below:
+            # bpy.ops.transform.rotate(
+            #   value=np.deg2rad(90), constraint_axis=(False, True, False), constraint_orientation='LOCAL')
+            # BUT make sure to deselect all, and select\active the shaft object
+
+        render.filepath = output_file.format(k_img)
+        bpy.ops.render.render(write_still=True)
+
+    # nice for debug
+    return panda
+
+
+if __name__ == "__main__":
+    run()
---
+++ src/amira_blender_rendering/blender_utils.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os.path as osp
+import numpy as np
+
+import bpy
+from mathutils import Vector
+
+from amira_blender_rendering import utils
+
+pkg_dir = utils.get_my_dir(__file__)
+assets_dir = osp.join(pkg_dir, "assets")
+
+logger = utils.get_logger()
+
+
+def clear_all_objects():
+    for scene in bpy.data.scenes:
+        for obj in scene.objects:
+            try:
+                scene.objects.unlink(obj)
+            except RuntimeError:
+                bpy.data.objects[obj.name].select = True
+                bpy.context.scene.objects.active = obj
+                bpy.ops.object.mode_set(mode='OBJECT')
+                scene.objects.unlink(obj)
+
+    for bpy_data_iter in (
+            bpy.data.objects,
+            bpy.data.meshes,
+            bpy.data.lamps,
+            bpy.data.cameras,
+    ):
+        for id_data in bpy_data_iter:
+            bpy_data_iter.remove(id_data)
+
+
+def delete_object(object_name):
+
+    if not isinstance(object_name, str):
+        try:
+            object_name = object_name.name
+        except AttributeError as err:
+            logger.error("Expecting name string but got: {}".format(object_name))
+            raise err
+
+    bpy.ops.object.select_all(action='DESELECT')
+    if object_name in bpy.data.objects:
+        try:
+            bpy.data.objects[object_name].select = True  # Blender 2.7x
+        except Exception:
+            # https://wiki.blender.org/wiki/Reference/Release_Notes/2.80/Python_API/Scene_and_Object_API
+            bpy.data.objects[object_name].select_set(True)  # Blender 2.8x
+    else:
+        logger.warning("could not find object {}".format(object_name))
+
+    meshes_to_remove = list()
+    for ob in bpy.context.selected_objects:
+        meshes_to_remove.append(ob.data)
+
+    bpy.ops.object.delete()
+
+    for mesh in meshes_to_remove:
+        try:  # might data be a non-mesh ?
+            bpy.data.meshes.remove(mesh)
+        except Exception as err:
+            logger.error(str(err))
+
+
+def set_image_texture(obj, image_path, material_name):
+
+    COL_WIDTH = 200
+
+    img = bpy.data.images.load(image_path)
+
+    if len(obj.data.uv_textures) == 0:
+        obj.data.uv_textures.new()
+
+    mat = bpy.data.materials.new(name=material_name)
+    mat.use_nodes = True
+    obj.data.materials.append(mat)
+
+    nodes = mat.node_tree.nodes
+    # TODO : might be unnecessary, check prepending ShaderNodeTexImage to default nodes
+    for node in nodes:
+        nodes.remove(node)
+
+    links = mat.node_tree.links
+
+    nodes.new(type='ShaderNodeTexImage')
+    nodes[-1].name = "Image"
+    nodes["Image"].location = (0, 0)
+    nodes["Image"].image = img
+
+    nodes.new(type='ShaderNodeBsdfDiffuse')
+    nodes[-1].name = "Diffuse"
+    nodes["Diffuse"].location = (COL_WIDTH, 0)
+
+    links.new(nodes["Diffuse"].inputs["Color"], nodes["Image"].outputs["Color"])
+
+    nodes.new(type='ShaderNodeOutputMaterial')
+    nodes[-1].name = "Out"
+    nodes["Out"].location = (2 * COL_WIDTH, 0)
+
+    links.new(nodes["Out"].inputs["Surface"], nodes["Diffuse"].outputs[0])
+
+
+def create_room_corner():
+
+    bpy.ops.mesh.primitive_plane_add(
+        radius=1,
+        location=(0.0, 0.0, 0.0),
+    )
+    floor = bpy.context.active_object
+    floor.name = "Floor"
+    set_image_texture(
+        floor,
+        osp.join(assets_dir, "concrete_cracked_03-color.png"),
+        "concrete_cracked_03",
+    )
+
+    bpy.ops.mesh.primitive_plane_add(
+        radius=1,
+        location=(0.0, -1.0, 1.0),
+        rotation=(0.5 * np.pi, 0.0, 0.0),
+    )
+    wall_1 = bpy.context.active_object
+    wall_1.name = "Wall_1"
+    set_image_texture(
+        wall_1,
+        osp.join(assets_dir, "brick_simple_45-color.jpg"),
+        "brick_simple_45",
+    )
+
+    bpy.ops.mesh.primitive_plane_add(
+        radius=1,
+        location=(-1.0, 0.0, 1.0),
+        rotation=(0.5 * np.pi, 0.0, 0.5 * np.pi),
+    )
+    wall_2 = bpy.context.active_object
+    wall_2.name = "Wall_2"
+    set_image_texture(
+        wall_2,
+        osp.join(assets_dir, "brick_simple_45-color.jpg"),
+        "brick_simple_45",
+    )
+
+
+def load_cad_part(cad_part):
+    current_objects = bpy.data.objects.keys()
+    blendfile = osp.join(assets_dir, "CAD_parts.blend")
+    bpy.ops.wm.append(filename=cad_part, directory=blendfile + "\\Object\\")
+    for o in bpy.data.objects.keys():
+        if o not in current_objects:
+            return bpy.data.objects[o]
+
+
+def translate_object(obj, translation: tuple):
+    obj.location += Vector(translation)
+
+
+def get_mesh_bounding_box(mesh):
+    """Returns Bounding-Box of Mesh-Object at zero position (Edit mode)"""
+    try:
+        xyz = mesh.data.vertices[0].co
+    except AttributeError as err:
+        print('expecting a mesh object, but no data.vertices attribute in object {}'.format(mesh))
+        raise err
+
+    bb = [[xyz[0], xyz[0]], [xyz[1], xyz[1]], [xyz[2], xyz[2]]]
+    for v in mesh.data.vertices:
+        xyz = v.co
+        for i in range(3):
+            bb[i][0] = min(bb[i][0], xyz[i])
+            bb[i][1] = max(bb[i][1], xyz[i])
+
+    bounding_box = BoundingBox3D(bb[0][0], bb[0][1], bb[1][0], bb[1][1], bb[2][0], bb[2][1])
+    return bounding_box
+
+
+class Range1D():
+
+    def __init__(self, _min, _max):
+        if _max < _min:
+            raise AssertionError('_max {} must be greater than _min {}'.format(_max, _min))
+        self.min = _min
+        self.max = _max
+
+
+class BoundingBox3D():
+
+    def __init__(self, x_min, x_max, y_min, y_max, z_min, z_max):
+        self.x = Range1D(x_min, x_max)
+        self.y = Range1D(y_min, y_max)
+        self.z = Range1D(z_min, z_max)
