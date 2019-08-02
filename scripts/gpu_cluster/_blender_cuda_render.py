09d26bd09ca39b69cc1b887c2a347e875790941d
Nicolai Waniek (CR/PJ-AI-R31) <Nicolai.Waniek@de.bosch.com>
2019-08-02 12:09:46 +0200

    set path correctly

--- scripts/gpu_cluster/_blender_cuda_render.py
+++ scripts/gpu_cluster/_blender_cuda_render.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import bpy
+import sys
+from tempfile import NamedTemporaryFile
 
 
 def activate_cuda_devices():
@@ -33,10 +35,41 @@ def activate_cuda_devices():
         for d in prefs.devices:
             print(d.name, d.use)
 
-        # enable GPU compute devices for the current scene
+        # using the current scene, enable GPU Compute for rendering
         bpy.context.scene.cycles.device = 'GPU'
 
+
+def get_output_path():
+    """This function tries to determine the output path that the user specified
+    (-o,--render-output). If nothing was specified, returns an empty string."""
+
+    # get the path from argv
+    argv = sys.argv
+    try:
+        index = argv.index('-o') + 1
+    except:
+        try:
+            index = argv.index('--render-output') + 1
+        except:
+            index = -1
+
+    if index == -1:
+        return ''
+    else:
+        return argv[index]
+
+
 def render_image():
+    """This function first determines and sets the output path, then renders the image"""
+
+    path = get_output_path()
+    if path == '':
+        path = NamedTemporaryFile(suffix='.png').name
+        print(f"WW: No output path specified via '-o,--render-output'. Will store result to {path}")
+
+    # set path
+    bpy.context.scene.render.filepath = path
+    # render
     bpy.ops.render.render(write_still=True)
 
 
