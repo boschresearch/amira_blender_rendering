73681300b72a3c9228b2cf89bafae5570d2768d9
Nicolai Waniek (CR/PJ-AI-R31) <Nicolai.Waniek@de.bosch.com>
2019-08-16 16:26:37 +0200

    rename material setup functions to setup_material

---
+++ src/amira_blender_rendering/nodes/material_3Dprinted_plastic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import bpy
+import logging
+from mathutils import Vector
+from amira_blender_rendering import utils
+from amira_blender_rendering.blender_utils import clear_orphaned_materials, remove_material_nodes, add_default_material
+
+# TODO: comments from material_metal_tool_cap.py apply
+
+
+def setup_material(material: bpy.types.Material):
+    pass
