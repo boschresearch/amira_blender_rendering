1779f10e4e73ce48209158747fac094906d398c5
Nicolai Waniek (CR/PJ-AI-R31) <Nicolai.Waniek@de.bosch.com>
2019-08-16 15:39:48 +0200

    refactored to have base class for RenderedObjects scenes

--- src/amira_blender_rendering/scenes/__init__.py
+++ src/amira_blender_rendering/scenes/__init__.py
@@ -1,3 +1,9 @@
+"""The scenes module contains scene managers for various setups."""
+
+# base classes
 from .basescenemanager import BaseSceneManager
 from .threepointlighting import ThreePointLighting
+from .renderedobjectsbase import RenderedObjectsBase
+
+# concrete scenes
 from .simpletoolcap import SimpleToolCap
