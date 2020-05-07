dbf82409558d662903dc7b536422b356bc694b88
Todescato Marco (CR/PJ-AI-R31) <marco.todescato@de.bosch.com>
2020-05-07 10:14:15 +0200

    rename

--- scripts/slurm/slurmjobs-deploy.sh
+++ scripts/slurm/slurmjobs-deploy.sh
@@ -6,5 +6,5 @@ BASENAME=${1?Error: no BASENAME for slurmbatch .sh script to deploy given}
 
 for f in `ls ./$BASENAME*.sh`; do
     echo "Deploying batch job associated to '$f'"
-	sbatch  $f
+	  sbatch  $f
 done
