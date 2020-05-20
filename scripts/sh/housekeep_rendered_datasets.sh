bff14486d9d1da443b501a453457124a209bcb08
Marco Todescato <marco.todescato@de.bosch.com>
2020-05-20 12:14:03 +0200

    minor changes to allow cudnn version

--- scripts/sh/housekeep_rendered_datasets.sh
+++ scripts/sh/housekeep_rendered_datasets.sh
@@ -5,53 +5,85 @@
 # compact all the "compatible" rendered datasets into one single directory.
 
 # required positional argument to select desired datasets basename
-BASENAME=${1?Error: no BASENAME for desride datasets to housekeep given}
+BASENAME=${1?Error: no given BASENAME for datasets to housekeep}
 
 # optional data storage directory path
 AMIRA_DATA_STORAGE=${2:-/data/Employee/$USER/slurm_results}
+
+# decide for simple upacking or more sofisticated behavior with renaming
+# of subdirectories
+SIMPLE=${3:-True}
+
 # optional name dataset are unpack to
-FROMNAME=${3:-PhIRM}
+FROMNAME=${4:-PhIRM}
 # optional final directory name to store entire dataset to
-OUTNAME=${4:-PhIRM}
+OUTNAME=${5:-PhIRM}
 
-echo ""
-echo "Running housekeeping with following configuration:"
-echo "BASENAME for .tar to unpack                : $BASENAME"
-echo "Storage directory for .tar balls           : $AMIRA_DATA_STORAGE"
-echo "Original (shared) name of partial datasets : $FROMNAME"
-echo "Final dataset directory name               : $OUTNAME"
-echo ""
-echo -n "Continue? [Y/N]: "
-read OK
-echo ""
-if [[ $OK == "Y" ]]; then
 
-    cd $AMIRA_DATA_STORAGE
+if [[ $SIMPLE == True ]]; then
 
-    for f in `ls ./$BASENAME*.tar`; do
-        # extract substring from tarball name
-        TONAME=${f:0:-11}.d
-        #unpacking
-        echo "Unpacking $f"
-        tar xf $f
-        # rename
-        echo "Renaming current $FROMNAME into $TONAME"
-        mv ./$FROMNAME ./$TONAME
-    done
+    echo ""
+    echo "Running (simple) unpacking with following configuration:"
+    echo "BASENAME for .tar to unpack                : $BASENAME"
+    echo "Storage directory for .tar balls           : $AMIRA_DATA_STORAGE"
+    echo ""
+    echo -n "Continue? [Y/N]: "
+    read OK
+    echo ""
+    if [[ $OK == "Y" ]]; then
 
-    # create final directory.
-    # This is done here since fromname and outname can be the same.
-    # They might clash in this case.
-    mkdir $OUTNAME
+        cd $AMIRA_DATA_STORAGE
 
-    # moving to final directory and pack
-    echo "Moving to final $OUTNAME directory and packing..."
-    mv $BASENAME*.d $OUTNAME
-    tar cf $OUTNAME.tar $OUTNAME
-
-    cd -
+        for f in `ls ./$BASENAME*.tar`; do
+            #unpacking
+            echo "Unpacking $f"
+            tar xf $f
+        done
+    else
+        echo "Aborting..."
+        exit 0
+    fi
 
 else
-    echo "Aborting..."
-    exit 0
+    echo ""
+    echo "Running (sofisticated) housekeeping with following configuration:"
+    echo "BASENAME for .tar to unpack                : $BASENAME"
+    echo "Storage directory for .tar balls           : $AMIRA_DATA_STORAGE"
+    echo "Original (shared) name of partial datasets : $FROMNAME"
+    echo "Final dataset directory name               : $OUTNAME"
+    echo ""
+    echo -n "Continue? [Y/N]: "
+    read OK
+    echo ""
+    if [[ $OK == "Y" ]]; then
+
+        cd $AMIRA_DATA_STORAGE
+
+        for f in `ls ./$BASENAME*.tar`; do
+            # extract substring from tarball name
+            TONAME=${f:0:-11}.d
+            #unpacking
+            echo "Unpacking $f"
+            tar xf $f
+            # rename
+            echo "Renaming current $FROMNAME into $TONAME"
+            mv ./$FROMNAME ./$TONAME
+        done
+
+        # create final directory.
+        # This is done here since fromname and outname can be the same.
+        # They might clash in this case.
+        mkdir $OUTNAME
+
+        # moving to final directory and pack
+        echo "Moving to final $OUTNAME directory and packing..."
+        mv $BASENAME*.d $OUTNAME
+        tar cf $OUTNAME.tar $OUTNAME
+
+        cd -
+
+    else
+        echo "Aborting..."
+        exit 0
+    fi
 fi
