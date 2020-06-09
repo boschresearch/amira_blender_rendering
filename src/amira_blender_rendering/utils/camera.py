0a2985201d36772f4c8b1a2133cc60086ea4338b
Pollak Zuckerman Liad <pll1tv@TV2ZOSD8.hfa.emea.bosch.com>
2020-06-09 16:10:42 +0300

    simple multiview works

--- src/amira_blender_rendering/utils/camera.py
+++ src/amira_blender_rendering/utils/camera.py
@@ -5,7 +5,10 @@ from mathutils import Vector, Matrix
 import warnings
 import numpy as np
 
-def opencv_to_blender(K, cam, scale = 1.0):
+from amira_blender_rendering.math.points_on_sphere import generate_points
+
+
+def opencv_to_blender(K, cam, scale=1.0):
     """Convert the intrinsic camera from OpenCV to blender's format
 
     Args:
@@ -18,18 +21,18 @@ def opencv_to_blender(K, cam, scale = 1.0):
     if K is None:
         return cam
 
-    sensor_width_mm  = K[1,1] * K[0,2] / (K[0,0] * K[1,2])
+    sensor_width_mm = K[1, 1] * K[0, 2] / (K[0, 0] * K[1, 2])
     sensor_height_mm = 1
 
     # assume principal point in center
-    pixel_size_x = K[0,2] * 2
-    pixel_size_y = K[1,2] * 2
-    pixel_aspect = K[0,0] / K[1, 1]
+    pixel_size_x = K[0, 2] * 2
+    pixel_size_y = K[1, 2] * 2
+    pixel_aspect = K[0, 0] / K[1, 1]
 
     s_u = pixel_size_x / sensor_width_mm
     s_v = pixel_size_y / sensor_height_mm
 
-    f_in_mm = K[0,0] / s_u
+    f_in_mm = K[0, 0] / s_u
 
     scene = bpy.context.scene
     scene.render.resolution_x = pixel_size_x / scale
@@ -46,7 +49,7 @@ def opencv_to_blender(K, cam, scale = 1.0):
     return cam
 
 
-def opengl_to_opencv(v : Vector) -> Vector:
+def opengl_to_opencv(v: Vector) -> Vector:
     """Turn a coordinate in OpenGL convention to OpenCV convention.
 
     OpenGL's (and blenders) coordinate system has x pointing right, y pointing
@@ -178,7 +181,7 @@ def get_intrinsics(scene, cam):
     # extract additional sensor information (size in mm, sensor fit)
     sensor_size_mm = cam.sensor_height if cam.sensor_fit == 'VERTICAL' else cam.sensor_width
     sensor_fit = get_sensor_fit(cam.sensor_fit, render.pixel_aspect_x * resolution_x,
-                                                render.pixel_aspect_y * resolution_y)
+                                render.pixel_aspect_y * resolution_y)
 
     # compute pixel size in mm per pixel
     pixel_aspect_ratio = render.pixel_aspect_y / render.pixel_aspect_x
@@ -196,3 +199,33 @@ def get_intrinsics(scene, cam):
     # finalize K
     return s_u, s_v, u_0, v_0
 
+
+def generate_locations_list(num_locations=30, camera_scale=1, camera_bias=(0, 0, 1.5)):
+    """
+    Creates a list of XYZ locations in half a sphere around and over (0, 0, 0)
+
+    Args:
+        num_locations: number of required locations on the unit half sphere around (0, 0, 0)
+        camera_scale, camera_bias: scale and biase relative to unit sphere
+
+    Returns:
+        half_sphere_locations (list): a list of locations
+    """
+
+    sphere_locations = generate_points(2 * num_locations)
+
+    if isinstance(camera_scale, int):
+        camera_scale = [camera_scale] * 3
+
+    if isinstance(camera_scale, int):
+        camera_bias = [camera_bias] * 3
+
+    half_sphere_locations = []
+    for loc in sphere_locations:
+        if loc[-1] >= 0:
+            loc = [camera_scale[i] * x + camera_bias[i] for i, x in enumerate(loc)]
+            half_sphere_locations.append(tuple(loc))
+
+    assert (len(half_sphere_locations) == num_locations)
+
+    return half_sphere_locations
