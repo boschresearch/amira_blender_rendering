cc18cd8674678851974e7d5b2e3ae14692103eb7
Todescato Marco (CR/PJ-AI-R31) <marco.todescato@de.bosch.com>
2020-10-29 11:44:59 +0100

    adapt scenes

--- src/amira_blender_rendering/scenes/workstationscenarios.py
+++ src/amira_blender_rendering/scenes/workstationscenarios.py
@@ -743,8 +743,7 @@ class WorkstationScenarios(interfaces.ABRScene):
                             bpy.context.scene.camera,
                             self.objs,
                             self.config.camera_info.zeroing,
-                            rectify_depth=self.config.postprocess.rectify_depth,
-                            visibility_from_mask=self.config.postprocess.visibility_from_mask)
+                            postprocess_config=self.config.postprocess)
 
                         if self.config.debug.enabled and self.config.debug.save_to_blend:
                             # reset frame to 0 and save
