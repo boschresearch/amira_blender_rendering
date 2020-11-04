2b598cc229dee578603dccb3fd85027fee70b0bb
Todescato Marco (CR/PJ-AI-R31) <marco.todescato@de.bosch.com>
2020-11-04 19:15:05 +0100

    new generation and deployment script

--- config/PhIRM-DLR/generate_configs.py
+++ config/PhIRM-DLR/generate_configs.py
@@ -10,10 +10,10 @@
 # dataset type
 modes = ['Detection', 'Tracking']
 
-# image count per detection / tracking configuration per camera
+# scene/view count per mode per camera
 images = {
-    modes[0]: 100,
-    modes[1]: 100
+    modes[0]: [100, 5],
+    modes[1]: [1, 100]
 }
 
 # Target object specification. We will simply enumerate the objects, so that we
@@ -33,7 +33,6 @@ target_objects = {
 }
 
 # object configurations, i.e. which objects to pair in which configuration.
-# TODO: change obj config
 target_obj_sets = {
     'A': [0, 1],
     'B': [2, 3],
@@ -48,35 +47,46 @@ target_obj_sets = {
 }
 
 target_scenes = {
-    'A': 'robottable_empty',
-    'B': 'robottable_empty',
-    'C': 'robottable_empty',
-    'D': 'robottable_empty',
-    'E': 'robottable_empty',
-    'F': 'robottable_distractors',
-    'G': 'robottable_distractors',
-    'H': 'robottable_distractors',
-    'I': 'robottable_distractors',
-    'L': 'robottable_distractors',
+    'A': 'robottable_empty_no_cameraframe',
+    'B': 'robottable_empty_no_cameraframe',
+    'C': 'robottable_empty_no_cameraframe',
+    'D': 'robottable_empty_no_cameraframe',
+    'E': 'robottable_empty_no_cameraframe',
+    'F': 'robottable_distractors_no_cameraframe',
+    'G': 'robottable_distractors_no_cameraframe',
+    'H': 'robottable_distractors_no_cameraframe',
+    'I': 'robottable_distractors_no_cameraframe',
+    'L': 'robottable_distractors_no_cameraframe',
 }
 
 # specify for which mode we want to produce multi-view output
 multi_view = {
-    modes[0]: False,
-    modes[1]: True
-}
-
-multi_view_mode = {
-    'A': 'wave',
-    'B': 'circle',
-    'C': 'wave',
-    'D': 'circle',
-    'E': 'wave',
-    'F': 'circle',
-    'G': 'wave',
-    'H': 'circle',
-    'I': 'wave',
-    'L': 'circle',
+    modes[0]: {
+        'enabled': True,
+        'A': 'linear',
+        'B': 'linear',
+        'C': 'linear',
+        'D': 'linear',
+        'E': 'linear',
+        'F': 'linear',
+        'G': 'linear',
+        'H': 'linear',
+        'I': 'linear',
+        'L': 'linear',
+    },
+    modes[1]: {
+        'enabled': True,
+        'A': 'wave',
+        'B': 'circle',
+        'C': 'wave',
+        'D': 'circle',
+        'E': 'wave',
+        'F': 'circle',
+        'G': 'wave',
+        'H': 'circle',
+        'I': 'wave',
+        'L': 'circle',
+    }
 }
 
 # specify for which mode we want to produce motion blur
@@ -88,10 +98,22 @@ motion_blur = {
 # specify how many frames we want to forward simulate each Configuration
 forward_frames = 50
 
+# specify object(s) in the scene whose textures are randommized
+textured_objs_sets = {
+    'A': {'objects': [], 'textures': ''},
+    'B': {'objects': [], 'textures': ''},
+    'C': {'objects': [], 'textures': ''},
+    'D': {'objects': [], 'textures': ''},
+    'E': {'objects': [], 'textures': ''},
+    'F': {'objects': ['RubberPlate'], 'textures': '$DATA/models/object_textures'},
+    'G': {'objects': ['RubberPlate'], 'textures': '$DATA/models/object_textures'},
+    'H': {'objects': ['RubberPlate'], 'textures': '$DATA/models/object_textures'},
+    'I': {'objects': ['RubberPlate'], 'textures': '$DATA/models/object_textures'},
+    'L': {'objects': ['RubberPlate'], 'textures': '$DATA/models/object_textures'},
+}
+
 # select which configurations to generate
-#target_configs = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'L']
-#target_configs = ['A', 'B']
-target_configs = ['L']
+target_configs = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'L']
 
 # name of dataset, used as target directory
 dataset_name = 'BoschDataset'
@@ -146,7 +168,6 @@ forward_frames = {fframes}
 
 # [postprocess]
 postprocess = """[postprocess]
-rectify_depth = True
 depth_scale = 1000.0
 compute_disparity = True
 parallel_cameras = Camera.FrontoParallel.Left, Camera.FrontoParallel.Right
@@ -158,14 +179,19 @@ parallel_cameras_baseline_mm = 50
 # [multiview_setup]
 def get_multiview_setup(mode: str = 'wave'):
     if mode == 'wave':
-        cfg_str = """mode_config.center = -0.6, 0.08, 1.6
-mode_config.radius = 0.44
+        cfg_str = """mode_config.center = -0.75, 0., 1.8
+mode_config.radius = 0.5
 mode_config.frequency = 4
-mode_config.amplitude = 0.2
+mode_config.amplitude = 0.28
 """
     elif mode == 'circle':
-        cfg_str = """mode_config.center = -0.6, 0.08, 1.3
-mode_config.radius = 0.44
+        cfg_str = """mode_config.center = -0.75, 0., 1.5
+mode_config.radius = 0.45
+"""
+    elif mode == 'linear':
+        cfg_str = """mode_config.p0 = 0, 0, 0
+mode_config.p1 = 0.1, 0, -0.5
+mode_config.offset =
 """
     else:
         ValueError(f'Unknown multiview mode {mode}')
@@ -182,24 +208,38 @@ with open('all_parts.cfg', 'r') as f:
 
 
 # [scenario_setup]
-def get_scenario_setup(parts: str):
+def get_scenario_setup(parts: str, txt_objs: str, textures: str):
+    """
+    Args:
+        parts(str): string with list of target objects to render
+        txt_objs(str): strin with list of object whose texture is randomized at render
+        textures(str): path to textures
+    """
     return f"""[scenario_setup]
 target_objects = {parts}
+textured_objects = {txt_objs}
+objects_textures = {textures}
 """
 
 
-def gen_config(C, trgt_objs_set):
+def gen_config(C, trgt_objs_set, txt_objs_set):
 
     # construct target objects
     targets = [f'{target_objects[obj_id][0]}:{target_objects[obj_id][1]}' for obj_id in trgt_objs_set]
     targets = ", ".join(targets)
 
+    # construct textured objects
+    textured_objects = ", ".join(txt_objs_set['objects'])
+    object_textures = txt_objs_set['textures']
+
     for mode in modes:
         base_path = f"$AMIRA_DATASETS/{dataset_name}/{mode}/Configuration{C}"
 
-        if multi_view[mode]:
-            dset_str = get_dataset(images[mode], base_path, nviews=images[mode])
-            multi_view_str = get_multiview_setup(multi_view_mode[C])
+        if multi_view[mode]['enabled']:
+            multi_view_mode = multi_view[mode][C]
+            dset_str = get_dataset(images[mode][0] * images[mode][1], base_path,
+                                   nscenes=images[mode][0], nviews=images[mode][1])
+            multi_view_str = get_multiview_setup(multi_view_mode)
         else:
             dset_str = get_dataset(images[mode], base_path, nscenes=images[mode])
             multi_view_str = ''
@@ -211,7 +251,7 @@ def gen_config(C, trgt_objs_set):
             + multi_view_str + '\n' \
             + postprocess + '\n' \
             + parts + '\n' \
-            + get_scenario_setup(targets)
+            + get_scenario_setup(targets, textured_objects, object_textures)
 
         fname = f"cfgs/tmp-{mode}-C{C}.cfg"
         with open(fname, 'w') as f:
@@ -221,4 +261,4 @@ def gen_config(C, trgt_objs_set):
 if __name__ == "__main__":
     for C in target_configs:
         print(f"Generating configs for Configuration {C}")
-        gen_config(C, target_obj_sets[C])
+        gen_config(C, target_obj_sets[C], textured_objs_sets[C])
