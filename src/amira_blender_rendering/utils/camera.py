fix depth conversion to avoid overflow

src/amira_blender_rendering/utils/camera.py
@@ -371,7 +371,11 @@ def project_pinhole_range_to_rectified_depth(filepath_in: str, filepath_out: str
     v_dirs_mtx = np.dot(K_inv, uv1).T.reshape(res_y, res_x, 3)
     v_dirs_mtx_unit_inv = np.reciprocal(np.linalg.norm(v_dirs_mtx, axis=2))
 
-    depth_img = (range_exr * v_dirs_mtx_unit_inv * scale).astype(np.uint16)
+    depth_img = (range_exr * v_dirs_mtx_unit_inv * scale)
+    # remove overflow values
+    depth_img[depth_img > 65000] = 0
+    # cast to 16 bit
+    depth_img = depth_img.astype(np.uint16)
 
     # write out if requested
     if filepath_out is not None: