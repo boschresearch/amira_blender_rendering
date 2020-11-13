6a5efdb0ffbba2bd41b14fc1fe631e5f55ec5acb
Todescato Marco (CR/PJ-AI-R31) <marco.todescato@de.bosch.com>
2020-11-13 11:02:50 +0100

    remove AMIRA related configs and scripts

--- config/PhIRM/render_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# go to top level folder
-pushd ../..
-for f in `ls config/PhIRM/Workstation-*.cfg`; do
-    echo "Rendering configuration file '$f'"
-    abrgen --config $f
-done
-popd
