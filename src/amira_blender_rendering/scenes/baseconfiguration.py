53d4596a41b0dd78d17759bfab68fa7ddb0816ea
Marco Todescato (CR/PJ-AI-R31) <Marco.Todescato@de.bosch.com>
2021-03-29 10:30:01 +0200

    remove camera config from baseconfig

--- src/amira_blender_rendering/scenes/baseconfiguration.py
+++ src/amira_blender_rendering/scenes/baseconfiguration.py
@@ -17,6 +17,7 @@
 # limitations under the License.
 
 from amira_blender_rendering.datastructures import Configuration
+from amira_blender_rendering.utils.camera import CameraGroupConfiguration
 
 
 class BaseConfiguration(Configuration):
@@ -34,28 +35,11 @@ class BaseConfiguration(Configuration):
         self.add_param('dataset.view_count', 1, 'Number of camera views per scene to generate')
         self.add_param('dataset.base_path', '', 'Path to storage directory')
         self.add_param('dataset.scene_type', '', 'Scene type')
-
-        # camera configuration
-        self.add_param('camera_info.name', 'Pinhole Camera', 'Name for the camera')
-        self.add_param('camera_info.model', 'pinhole', 'Camera model type')
-        self.add_param('camera_info.width', 640, 'Rendered image resolution (pixel) along x (width)')
-        self.add_param('camera_info.height', 480, 'Rendered image resolution (pixel) along y (height)')
-        self.add_param('camera_info.zeroing', [0.0, 0.0, 0.0], 'Default camera zeroing rotation in degrees')
-        self.add_param('camera_info.intrinsic', [],
-                       'camera intrinsics fx, fy, cx, cy, possible altered via blender during runtime.'
-                       ' If not available, leave empty.',
-                       special='maybe_list')
-        self.add_param('camera_info.sensor_width', 0.0, 'Sensor width in mm (if not available, set to 0.0)')
-        self.add_param('camera_info.focal_length', 0.0, 'Focal length in mm (if not available, set to 0.0)')
-        self.add_param('camera_info.hfov', 0.0,
-                       'Horizontal Field-of-View of the camera in degrees (if not available, set to 0.0)')
-        self.add_param('camera_info.intrinsics_conversion_mode', 'mm',
-                       'Determine how to compute camera setup from intrinsics. One of "fov", "mm".')
-
-        # self.add_param('camera_info.original_intrinsic', [],
-        # 'Camera intrinsics that were passed originaly as camera_info.intrinsic', special='maybe_list')
+        self.add_param('default_camera_group', CameraGroupConfiguration(), 'configuration for camera group')
 
         # render configuration
+        self.add_param('render_setup.width', 640, 'Rendered image resolution (pixel) along x (width)')
+        self.add_param('render_setup.height', 480, 'Rendered image resolution (pixel) along y (height)')
         self.add_param('render_setup.backend', 'blender-cycles', 'Render backend. Blender only one supported')
         self.add_param('render_setup.integrator', 'BRANCHED_PATH',
                        'Integrator used during path tracing. Either of PATH, BRANCHED_PATH')
@@ -75,9 +59,3 @@ class BaseConfiguration(Configuration):
         self.add_param('postprocess.visibility_from_mask', False,
                        'If True, if an invalid (empty) mask is found during postprocessing,'
                        ' object visibility info are overwritten to false')
-        self.add_param('postprocess.parallel_cameras', [],
-                       'Pair of parallel stereo cameras (among scene_setup.cameras) to postprocess')
-        self.add_param('postprocess.compute_disparity', False,
-                       'If True, toggle computation of disparity map (from depth) based on given baseline (mm) value')
-        self.add_param('postprocess.parallel_cameras_baseline_mm', 0,
-                       'Baseline value (i.e., translation) between parallel cameras locations (in mm). Default: 0')
